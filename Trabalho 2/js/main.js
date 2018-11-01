c79207ddacc3116c8ace83e1ecbfe440e2d29b35
FagnerSI <fagner314@gmail.com>
2018-11-01 13:18:56 -0300

    Trabalho concluido

--- Trabalho 2/js/main.js
+++ Trabalho 2/js/main.js
@@ -54,11 +54,50 @@ let jsonGroups = {
     ]
 }
 
+var groupAtive = undefined;
+let url = "http://rest.learncode.academy/api/fagner/groups";
+let url_msg = "http://rest.learncode.academy/api/fagner/";
+
 let list_group = document.querySelector(".list-group");
 let body_chat = document.querySelector(".body-chat"); 
 
-let url = "http://rest.learncode.academy/api/lms/groups";
-let url_msg = "http://rest.learncode.academy/api/lms/";
+let login = document.querySelector(".header .login");
+let modal = document.querySelector(".modal");
+let btn_user = document.querySelector(".modal .painel-modal .btn-user");
+let user_logado = document.querySelector(".header .user-name")
+user_logado.innerHTML = localStorage.getItem("user");
+
+
+
+if(localStorage.getItem("user")){
+    login.innerHTML = "Sair";
+}
+
+login.addEventListener("click", function(){
+   
+    if(localStorage.getItem("user")){
+        localStorage.removeItem("user");
+        login.innerHTML = "Entrar";
+        user_logado.innerHTML = "";
+        window.location.reload();
+    }
+    else{
+        modal.classList.add("modal-ativo");     
+   }
+})
+
+
+
+btn_user.addEventListener("click", function(){
+    
+    let input_user = document.querySelector(".modal .painel-modal .input-user");
+    localStorage.user = input_user.value;   
+    login.innerHTML = "Sair";
+    user_logado.innerHTML = localStorage.getItem("user");
+    modal.classList.remove("modal-ativo");
+    window.location.reload();
+    
+});
 
 function createComponentGroup(title_group, id_group) {
     let li = document.createElement("li");
@@ -78,14 +117,17 @@ function createComponentGroup(title_group, id_group) {
     list_group.appendChild(li);
 }
 
-function createComponentMsg(title_chat, message) {            
-    
+function createComponentMsg(msgp) {            
+    let title_chat = msgp.userName;
+    let message = msgp.message;
+
     let painel = document.createElement("div");
     let title = document.createElement("span");
     let body = document.createElement("p");
-
+   
     let group_msg = document.createTextNode(title_chat);
     let msg = document.createTextNode(message);
+
 
     title.appendChild(group_msg);
     body.appendChild(msg);
@@ -103,16 +145,17 @@ function addGroup() {
         
         if(request.readyState == 4){                    
             let resultRequest = JSON.parse(request.responseText);         
-            
+            console.log(resultRequest.length)
             if(resultRequest.length){
                 list_group.innerHTML = "";
                 body_chat.innerHTML = "";
-                
-                for (let i = 0; i < resultRequest[0].grupos.length; i++) {                       
-                    createComponentGroup(resultRequest[0].grupos[i].groupName, resultRequest[0].grupos[i].groupID);
+             
+                for (let i = 0; i < resultRequest.length; i++) {                       
+                    createComponentGroup(resultRequest[i].groupName, resultRequest[i].groupID);
                 }
                 event_click_group();
-            }         
+            }  
+               
         } 
                        
     };
@@ -123,25 +166,20 @@ function addGroup() {
     // for (let i = 0; i < jsonGroups.grupos.length; i++) { createComponentGroup(jsonGroups.grupos[i].grupo);};                        
         
 }
-addGroup();
 
 function openMsgs(groupId, groupName){
     let request = new XMLHttpRequest();
     let group_chat = document.querySelector(".chat .header-chat .group-chat");
-             group_chat.innerHTML = groupName;
+            group_chat.innerHTML = groupName;
             
-   
+
     request.onreadystatechange = function(){
         if(request.readyState == 4){
             let resultRequest = JSON.parse(request.responseText);
-                               
-            for (let i = 0; i < resultRequest[0].mensagens.length; i++) { 
-                  let msg = resultRequest[0].mensagens[i];        
-                  createComponentMsg(msg.usuario, msg.texto);                         
-            }
+            resultRequest.forEach(createComponentMsg);              
         }
     };
-   
+
     request.open("GET", url_msg+groupId, true);
     request.send();            
 
@@ -151,17 +189,79 @@ function openMsgs(groupId, groupName){
     //               jsonGroups.grupos[i].mensagens[i].texto );};};   
 }  
 
-function event_click_group(){  
+if(localStorage.getItem("user")){
+   
+    addGroup();    
 
-    let click_group = document.querySelectorAll(".menu-lateral ul li")
-    for(let i = 0; i < click_group.length; i++ ){
-       
-        click_group[i].addEventListener("click", function(){                   
-            let groupId = (click_group[i].lastChild.value);
-            let groupName = (click_group[i].childNodes[1].textContent);
-            body_chat.innerHTML = "";
-            openMsgs(groupId, groupName); 
-        });
+    function event_click_group(){  
+
+        let click_group = document.querySelectorAll(".menu-lateral ul li")
+        for(let i = 0; i < click_group.length; i++ ){
+        
+            click_group[i].addEventListener("click", function(){                   
+                let groupId = (click_group[i].lastChild.value);
+                groupAtive = groupId;
+                let groupName = (click_group[i].childNodes[1].textContent);
+                body_chat.innerHTML = "";
+                openMsgs(groupId, groupName); 
+            });
+        }
+
     }
 
+    let btn_msg = document.querySelector(".chat .msg-chat .btn-msg");
+    btn_msg.addEventListener("click", function(){
+        let input_msg = document.querySelector(".chat .msg-chat .input-msg");
+        if(groupAtive){
+            send_msg(groupAtive, input_msg.value);
+            input_msg.value = "";
+    }
+        
+    });
+
+    function send_msg(groupID, content){
+        console.log(url_msg+groupID)
+        let xmlr = new XMLHttpRequest;
+
+        xmlr.onreadystatechange = function(){
+            if(xmlr.readyState == 4){
+                let data = JSON.parse(xmlr.responseText)
+                createComponentMsg(data)
+            }
+        }
+        var user = localStorage.getItem("user");
+        var messag = {userName:user, message:content}
+    
+        xmlr.open("POST", url_msg+groupID, true);
+        xmlr.setRequestHeader("Content-type", "application/json");
+        xmlr.send(JSON.stringify(messag));
+    }
+
+    let btn_menu_group = document.querySelector(".menu-lateral .menu-gp .menu-btn");
+    btn_menu_group.addEventListener("click", function(){
+        let input_group = document.querySelector(".menu-lateral .menu-gp .menu-input");
+        let input_group_id = document.querySelector(".menu-lateral .menu-gp .menu-input-id");
+        create_group(input_group_id.value, input_group.value);  
+        input_group.value = "";
+        input_group_id.value = "";
+    });
+
+    function create_group(groupID, groupName){
+        
+        let xmlr = new XMLHttpRequest;
+
+        xmlr.onreadystatechange = function(){
+            if(xmlr.readyState == 4){
+                let data = JSON.parse(xmlr.responseText)
+                
+                addGroup();          
+            }        
+        }
+        var user = localStorage.getItem("user");
+        var data_send= {groupID:groupID, groupName:groupName}
+    
+        xmlr.open("POST", url, true);
+        xmlr.setRequestHeader("Content-type", "application/json");
+        xmlr.send(JSON.stringify(data_send));
+    }
 }
